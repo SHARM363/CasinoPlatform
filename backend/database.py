a13d7ad3ce32e9f06455d0e608b70afbff58cc9d
SHARM363 <shakilkhanm301@gmail.com>
2026-09-06 10:52:38 +0600

    Add PostgreSQL connection function

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,10 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from config import Config
 
+
+def get_connection():
+    return psycopg2.connect(
+        Config.DATABASE_URL,
+        cursor_factory=RealDictCursor
+    )
