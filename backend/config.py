828e61ad2ee0ebb3368db296e8a871d0663debbf
SHARM363 <shakilkhanm301@gmail.com>
2026-09-22 21:15:00 +0600

    Delete backend directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "casino-secret-key")
-    ADMIN_SECRET_KEY = os.getenv("ADMIN_SECRET_KEY")
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    DEBUG = os.getenv("DEBUG", "False").lower() == "true"
